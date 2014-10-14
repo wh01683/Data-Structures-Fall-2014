2aa7cbdabb8097812240f7f612bfb8ccd7bf6e5d
Robert Howerton <wh01683@georgiasouthern.edu>
2014-10-14 12:57:47 -0400

    Create linkedList.java

---
+++ linkedList.java
@@ -0,0 +1,41 @@
+/*
+
+*/
+
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		
+	}
+}
+
+class linkedQueue{
+	
+static int size;
+
+static linkedQueue[] linkedQueue(int z){
+	
+	for (i = 0; i< z; i++){
+		
+	}
+	
+}
+	
+static int size(){
+	//returns number of elements in the queue
+	int num;
+	for (i = 0; i<)
+}
+	
+	
+}
